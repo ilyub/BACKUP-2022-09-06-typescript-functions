5a10be3a8eb73fdb3c59e37c986b0e7770459ac2
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2021-11-18 22:06:57 +0300

    chore(fix,jest): Fix after "@skylib/config" update

--- jest.setup-after-env.ts
+++ jest.setup-after-env.ts
@@ -1,11 +1,9 @@
-import "@skylib/config/src/jest-globals";
-
 import * as testUtils from "./src/testUtils";
 
 testUtils.jestSetup();
 beforeEach(testUtils.jestReset);
 
-if (TEST_ENV === "jsdom") {
+if (JEST_ENV === "jsdom") {
   testUtils.jestSetup.dom();
   beforeEach(testUtils.jestReset.dom);
 }
