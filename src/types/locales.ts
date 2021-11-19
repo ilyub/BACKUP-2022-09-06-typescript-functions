503741cb4f7a431a539c5b8b54ffc20f1661d16a
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2021-11-19 17:55:14 +0300

    feat(types,locales): Removed "LocaleNameVO" constant

--- src/types/locales.ts
+++ src/types/locales.ts
@@ -1,5 +1,3 @@
-import { createValidationObject } from "./core";
-
 export type LocaleName =
   | "de-DE"
   | "en-GB"
@@ -7,12 +5,3 @@ export type LocaleName =
   | "es-ES"
   | "fr-FR"
   | "ru-RU";
-
-export const LocaleNameVO = createValidationObject<LocaleName>({
-  "de-DE": "de-DE",
-  "en-GB": "en-GB",
-  "en-US": "en-US",
-  "es-ES": "es-ES",
-  "fr-FR": "fr-FR",
-  "ru-RU": "ru-RU"
-});
--- tests/types/locales.ts
+++
@@ -1,7 +0,0 @@
-import * as is from "@/guards";
-import { LocaleNameVO } from "@/types/locales";
-
-it("LocaleNameVO", () => {
-  expect(is.enumeration("en-US", LocaleNameVO)).toBeTrue();
-  expect(is.enumeration(1, LocaleNameVO)).toBeFalse();
-});
