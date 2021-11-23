83e874b969953a5b9682f9ba0e3724128defe3c2
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2021-11-23 14:32:22 +0300

    feat(types,locales): Configurable "LocaleName" type

--- src/types/locales.ts
+++ src/types/locales.ts
@@ -1,7 +1,14 @@
-export type LocaleName =
-  | "de-DE"
-  | "en-GB"
-  | "en-US"
-  | "es-ES"
-  | "fr-FR"
-  | "ru-RU";
+declare global {
+  namespace configurable {
+    interface DefaultLocaleName {
+      readonly "en-US": true;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface LocaleName {}
+  }
+}
+
+export type LocaleName = keyof configurable.LocaleName extends never
+  ? keyof configurable.DefaultLocaleName
+  : keyof configurable.LocaleName;
