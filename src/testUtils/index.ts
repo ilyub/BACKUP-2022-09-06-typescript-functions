179ab3c1d1712a161d9195bfda2254f6443f9c9a
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2021-12-15 20:40:10 +0300

    feat(testUtils): Removed "htmlToEqual", "textToEqual", and "toExist" functions

--- src/testUtils/index.ts
+++ src/testUtils/index.ts
@@ -10,7 +10,6 @@ import * as fakeTimers from "@sinonjs/fake-timers";
 import * as a from "../array";
 import * as assert from "../assertions";
 import * as fn from "../function";
-import * as is from "../guards";
 import type { Async, DeepReadonly, PromiseAsync } from "../types/core";
 
 declare global {
@@ -36,32 +35,12 @@ declare global {
        */
       readonly executionTimeToEqual: (time: number) => Promise<R>;
       /**
-       * Checks that object contains expected HTML code.
-       *
-       * @param expected - Expected HTML code.
-       * @returns Result object.
-       */
-      readonly htmlToEqual: (expected: string) => R;
-      /**
-       * Checks that object contains expected text.
-       *
-       * @param expected - Expected text.
-       * @returns Result object.
-       */
-      readonly textToEqual: (expected: string) => R;
-      /**
        * Checks that two objects are identical.
        *
        * @param expected - Expected object.
        * @returns Result object.
        */
       readonly toBeSameAs: (expected: object) => R;
-      /**
-       * Checks that object exists.
-       *
-       * @returns Result object.
-       */
-      readonly toExist: () => R;
     }
   }
 }
@@ -165,39 +144,6 @@ export function getClock(): DeepReadonly<fakeTimers.Clock> {
 }
 
 /**
- * Checks that object contains expected HTML code.
- *
- * @param got - Got value.
- * @param expected - Expected HTML code.
- * @returns Result object.
- */
-export function htmlToEqual(got: unknown, expected: string): ExpectReturnType {
-  const html = fn
-    .run(() => {
-      if (got instanceof HTMLElement) return got.innerHTML;
-
-      const isHtml: is.Guard<() => string> = is.callable;
-
-      assert.object.of(got, { html: isHtml }, {}, 'Missing "html" method');
-
-      return got.html();
-    })
-    .trim()
-    .replace(/\s+/gu, " ");
-
-  return html === expected
-    ? {
-        message: (): string => `Expected HTML code not to be "${expected}"`,
-        pass: true
-      }
-    : {
-        message: (): string =>
-          `Expected HTML code to be "${expected}", got "${html}"`,
-        pass: false
-      };
-}
-
-/**
  * Installs fake timer.
  *
  * @param options - Options.
@@ -237,19 +183,13 @@ export function jestSetup(): void {
     interface ExpectExtendMap {
       readonly executionTimeToBeWithin: ExpectFromMatcher<"executionTimeToBeWithin">;
       readonly executionTimeToEqual: ExpectFromMatcher<"executionTimeToEqual">;
-      readonly htmlToEqual: ExpectFromMatcher<"htmlToEqual">;
-      readonly textToEqual: ExpectFromMatcher<"textToEqual">;
       readonly toBeSameAs: ExpectFromMatcher<"toBeSameAs">;
-      readonly toExist: ExpectFromMatcher<"toExist">;
     }
 
     const expectExtend: ExpectExtendMap = {
       executionTimeToBeWithin,
       executionTimeToEqual,
-      htmlToEqual,
-      textToEqual,
-      toBeSameAs,
-      toExist
+      toBeSameAs
     };
 
     expect.extend(matchers as jest.ExpectExtendMap);
@@ -305,43 +245,6 @@ export function setRandomSystemTime(): void {
 }
 
 /**
- * Checks that object contains expected text.
- *
- * @param got - Got value.
- * @param expected - Expected text.
- * @returns Result object.
- */
-export function textToEqual(got: unknown, expected: string): ExpectReturnType {
-  const text = fn
-    .run(() => {
-      if (got instanceof HTMLElement) {
-        assert.not.empty(got.textContent);
-
-        return got.textContent;
-      }
-
-      const isText: is.Guard<() => string> = is.callable;
-
-      assert.object.of(got, { text: isText }, {}, 'Missing "text" method');
-
-      return got.text();
-    })
-    .trim()
-    .replace(/\s+/gu, " ");
-
-  return text === expected
-    ? {
-        message: (): string => `Expected text not to be "${expected}"`,
-        pass: true
-      }
-    : {
-        message: (): string =>
-          `Expected text to be "${expected}", got "${text}"`,
-        pass: false
-      };
-}
-
-/**
  * Checks that two objects are identical.
  *
  * @param got - Got value.
@@ -356,32 +259,6 @@ export function toBeSameAs(got: unknown, expected: object): ExpectReturnType {
       }
     : {
         message: (): string => "Expected the same object",
-        pass: false
-      };
-}
-
-/**
- * Checks that object exists.
- *
- * @param got - Got value.
- * @returns Result object.
- */
-export function toExist(got: unknown): ExpectReturnType {
-  const exists = fn.run(() => {
-    const isExists: is.Guard<() => boolean> = is.callable;
-
-    assert.object.of(got, { exists: isExists }, {}, 'Missing "exists" method');
-
-    return got.exists();
-  });
-
-  return exists
-    ? {
-        message: (): string => "Expected object not to exist",
-        pass: true
-      }
-    : {
-        message: (): string => "Expected object to exist",
         pass: false
       };
 }
--- tests/testUtils/index.ts
+++ tests/testUtils/index.ts
@@ -55,34 +55,6 @@ it("getClock", () => {
   expect(testUtils.getClock()).toBeObject();
 });
 
-it("htmlToEqual", () => {
-  const jq = $("<div>").append($("<div>").html("sample-text"));
-
-  for (const result of [
-    testUtils.htmlToEqual(jq, "<div>sample-text</div>"),
-    testUtils.htmlToEqual(jq.get(0), "<div>sample-text</div>")
-  ]) {
-    expect(result.pass).toBeTrue();
-    expect(result.message()).toStrictEqual(
-      'Expected HTML code not to be "<div>sample-text</div>"'
-    );
-  }
-
-  for (const result of [
-    testUtils.htmlToEqual(jq, "<div>sample-text-2</div>"),
-    testUtils.htmlToEqual(jq.get(0), "<div>sample-text-2</div>")
-  ]) {
-    expect(result.pass).toBeFalse();
-    expect(result.message()).toStrictEqual(
-      'Expected HTML code to be "<div>sample-text-2</div>", got "<div>sample-text</div>"'
-    );
-  }
-
-  expect(() => testUtils.htmlToEqual({}, "")).toThrow(
-    new Error('Missing "html" method')
-  );
-});
-
 it("jestReset", () => {
   const callback = jest.fn();
 
@@ -112,34 +84,6 @@ it("setRandomSystemTime", () => {
   testUtils.setRandomSystemTime();
 });
 
-it("textToEqual", () => {
-  const jq = $("<div>").html("sample-text");
-
-  for (const result of [
-    testUtils.textToEqual(jq, "sample-text"),
-    testUtils.textToEqual(jq.get(0), "sample-text")
-  ]) {
-    expect(result.pass).toBeTrue();
-    expect(result.message()).toStrictEqual(
-      'Expected text not to be "sample-text"'
-    );
-  }
-
-  for (const result of [
-    testUtils.textToEqual(jq, "sample-text-2"),
-    testUtils.textToEqual(jq.get(0), "sample-text-2")
-  ]) {
-    expect(result.pass).toBeFalse();
-    expect(result.message()).toStrictEqual(
-      'Expected text to be "sample-text-2", got "sample-text"'
-    );
-  }
-
-  expect(() => testUtils.textToEqual({}, "")).toThrow(
-    new Error('Missing "text" method')
-  );
-});
-
 it("toBeSameAs", () => {
   {
     const arr1 = [1];
@@ -167,32 +111,4 @@ it("toBeSameAs", () => {
     expect(obj1).not.toBeSameAs(obj2);
     expect(obj1).toStrictEqual(obj2);
   }
-});
-
-it("toExist", () => {
-  {
-    const result = testUtils.toExist({
-      exists() {
-        return true;
-      }
-    });
-
-    expect(result.pass).toBeTrue();
-    expect(result.message()).toStrictEqual("Expected object not to exist");
-  }
-
-  {
-    const result = testUtils.toExist({
-      exists() {
-        return false;
-      }
-    });
-
-    expect(result.pass).toBeFalse();
-    expect(result.message()).toStrictEqual("Expected object to exist");
-  }
-
-  expect(() => testUtils.toExist({})).toThrow(
-    new Error('Missing "exists" method')
-  );
 });
