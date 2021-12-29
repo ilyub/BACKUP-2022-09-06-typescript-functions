1afdec972cb3f90ab2ca9fae0412a060e168d491
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2021-12-29 12:59:21 +0300

    feat(json): Allow to encode maps and sets

--- src/json.ts
+++ src/json.ts
@@ -1,7 +1,10 @@
 /* skylib/eslint-plugin disable @skylib/disallow-identifier[json] */
 
+import * as a from "./array";
+import * as assert from "./assertions";
 import * as is from "./guards";
 import type { stringE } from "./types/core";
+import { createValidationObject } from "./types/core";
 
 /**
  * Decodes JSON string.
@@ -11,7 +14,7 @@ import type { stringE } from "./types/core";
  */
 export function decode(source: stringE): unknown {
   try {
-    if (is.not.empty(source)) return JSON.parse(source);
+    if (is.not.empty(source)) return JSON.parse(source, reviver);
   } catch {
     //
   }
@@ -36,8 +39,32 @@ export function encode(source: unknown): string {
 |*******************************************************************************
 |*/
 
+type DataType = "map-5702-3c89-3feb-75d4" | "set-41ef-10c9-ae1f-15e8";
+
+const DataTypeVO = createValidationObject<DataType>({
+  "map-5702-3c89-3feb-75d4": "map-5702-3c89-3feb-75d4",
+  "set-41ef-10c9-ae1f-15e8": "set-41ef-10c9-ae1f-15e8"
+});
+
+const isDataType = is.factory(is.enumeration, DataTypeVO);
+
+const isCustomData = is.factory(
+  is.object.of,
+  {
+    dataType: isDataType,
+    value: is.unknown
+  },
+  {}
+);
+
+const isMapEntry = is.tuple.factory(is.unknown, is.unknown);
+
+const isMapValue = is.factory(is.array.of, isMapEntry);
+
+const isSetValue = is.factory(is.array);
+
 /**
- * JSON replacer. Replaces _undefined_ with _null_.
+ * JSON replacer.
  *
  * @param _key - Key.
  * @param value - Value.
@@ -45,5 +72,44 @@ export function encode(source: unknown): string {
  */
 function replacer(_key: unknown, value: unknown): unknown {
   // eslint-disable-next-line unicorn/no-null
-  return is.not.empty(value) ? value : null;
+  if (is.empty(value)) return null;
+
+  if (value instanceof Map)
+    return {
+      dataType: "map-5702-3c89-3feb-75d4",
+      value: a.fromIterable(value)
+    };
+
+  if (value instanceof Set)
+    return {
+      dataType: "set-41ef-10c9-ae1f-15e8",
+      value: a.fromIterable(value)
+    };
+
+  return value;
+}
+
+/**
+ * JSON reviver.
+ *
+ * @param _key - Key.
+ * @param value - Value.
+ * @returns New value.
+ */
+function reviver(_key: unknown, value: unknown): unknown {
+  // eslint-disable-next-line unicorn/no-null
+  if (is.empty(value)) return null;
+
+  if (isCustomData(value))
+    switch (value.dataType) {
+      case "map-5702-3c89-3feb-75d4":
+        assert.byGuard(value.value, isMapValue);
+        return new Map(value.value);
+
+      case "set-41ef-10c9-ae1f-15e8":
+        assert.byGuard(value.value, isSetValue);
+        return new Set(value.value);
+    }
+
+  return value;
 }
--- tests/json.ts
+++ tests/json.ts
@@ -7,6 +7,18 @@ it("decode", () => {
   expect(json.decode("null")).toBeNull();
   expect(json.decode("[null]")).toStrictEqual([null]);
   expect(json.decode('{"a":1}')).toStrictEqual({ a: 1 });
+  expect(
+    json.decode('{"dataType":"map-5702-3c89-3feb-75d4","value":[[1,"a"]]}')
+  ).toStrictEqual(new Map([[1, "a"]]));
+  expect(
+    json.decode('{"dataType":"map-5702-3c89-3feb-75d4","value":[["a",1]]}')
+  ).toStrictEqual(new Map([["a", 1]]));
+  expect(
+    json.decode('{"dataType":"set-41ef-10c9-ae1f-15e8","value":[1,"a"]}')
+  ).toStrictEqual(new Set([1, "a"]));
+  expect(
+    json.decode('{"dataType":"set-41ef-10c9-ae1f-15e8","value":["a",1]}')
+  ).toStrictEqual(new Set(["a", 1]));
 });
 
 it("encode", () => {
@@ -14,4 +26,16 @@ it("encode", () => {
   expect(json.encode(undefined)).toStrictEqual("null");
   expect(json.encode([null, undefined])).toStrictEqual("[null,null]");
   expect(json.encode({ a: 1 })).toStrictEqual('{"a":1}');
+  expect(json.encode(new Map([[1, "a"]]))).toStrictEqual(
+    '{"dataType":"map-5702-3c89-3feb-75d4","value":[[1,"a"]]}'
+  );
+  expect(json.encode(new Map([["a", 1]]))).toStrictEqual(
+    '{"dataType":"map-5702-3c89-3feb-75d4","value":[["a",1]]}'
+  );
+  expect(json.encode(new Set([1, "a"]))).toStrictEqual(
+    '{"dataType":"set-41ef-10c9-ae1f-15e8","value":[1,"a"]}'
+  );
+  expect(json.encode(new Set(["a", 1]))).toStrictEqual(
+    '{"dataType":"set-41ef-10c9-ae1f-15e8","value":["a",1]}'
+  );
 });
