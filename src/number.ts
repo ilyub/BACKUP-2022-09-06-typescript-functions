2c76be797ea17a1dd01f51b6a4b3228f5d65abd3
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-03-09 23:18:12 +0300

    feat(number): Added "ceil", "floor", and "limit" functions

--- src/number.ts
+++ src/number.ts
@@ -1,19 +1,6 @@
 import * as assert from "./assertions";
 
 /**
- * Rounds number to a given precision.
- *
- * @param value - Value to be rounded.
- * @param precision - The number of digits to keep.
- * @returns Rounded value.
- */
-export function round(value: number, precision: number): number {
-  const divider = 10 ** precision;
-
-  return Math.round(value * divider) / divider;
-}
-
-/**
  * Averages values.
  *
  * @param values - Values.
@@ -24,6 +11,99 @@ export function average(...values: number[]): number {
 
   return sum(...values) / values.length;
 }
+
+/**
+ * Ceils number to a given precision.
+ *
+ * @param value - Value.
+ * @param precision - The number of digits to keep.
+ * @returns Ceiled value.
+ */
+export function ceil(value: number, precision: number): number {
+  const divider = 10 ** precision;
+
+  return Math.ceil(value * divider) / divider;
+}
+
+/**
+ * Ceils number.
+ *
+ * @param value - Value.
+ * @param step - Step.
+ * @param from - From.
+ * @returns Ceiled value.
+ */
+export function ceilStep(value: number, step: number, from = 0): number {
+  return Math.ceil((value - from) / step) * step + from;
+}
+
+ceil.step = ceilStep;
+
+/**
+ * Floors number to a given precision.
+ *
+ * @param value - Value.
+ * @param precision - The number of digits to keep.
+ * @returns Floored value.
+ */
+export function floor(value: number, precision: number): number {
+  const divider = 10 ** precision;
+
+  return Math.floor(value * divider) / divider;
+}
+
+/**
+ * Floors number.
+ *
+ * @param value - Value.
+ * @param step - Step.
+ * @param from - From.
+ * @returns Floored value.
+ */
+export function floorStep(value: number, step: number, from = 0): number {
+  return Math.floor((value - from) / step) * step + from;
+}
+
+floor.step = floorStep;
+
+/**
+ * Limits value to be within [min, max] range.
+ *
+ * @param value - Value.
+ * @param min - Min.
+ * @param max - Max.
+ * @returns Limited value.
+ */
+export function limit(value: number, min: number, max: number): number {
+  return Math.min(Math.max(value, min), max);
+}
+
+/**
+ * Rounds number to a given precision.
+ *
+ * @param value - Value.
+ * @param precision - The number of digits to keep.
+ * @returns Rounded value.
+ */
+export function round(value: number, precision: number): number {
+  const divider = 10 ** precision;
+
+  return Math.round(value * divider) / divider;
+}
+
+/**
+ * Rounds number.
+ *
+ * @param value - Value.
+ * @param step - Step.
+ * @param from - From.
+ * @returns Rounded value.
+ */
+export function roundStep(value: number, step: number, from = 0): number {
+  return Math.round((value - from) / step) * step + from;
+}
+
+round.step = roundStep;
 
 /**
  * Sums values.
--- tests/number.ts
+++ tests/number.ts
@@ -8,10 +8,48 @@ test("average", () => {
   expect(() => num.average()).toThrow(new AssertionError());
 });
 
+test("ceil", () => {
+  expect(num.ceil(1.111, 1)).toBe(1.2);
+  expect(num.ceil(1.199, 1)).toBe(1.2);
+  expect(num.ceil(1.111, 2)).toBe(1.12);
+  expect(num.ceil(1.119, 2)).toBe(1.12);
+});
+
+test("ceil.step", () => {
+  expect(num.ceil.step(11, 10)).toBe(20);
+  expect(num.ceil.step(19, 10)).toBe(20);
+});
+
+test("floor", () => {
+  expect(num.floor(1.111, 1)).toBe(1.1);
+  expect(num.floor(1.199, 1)).toBe(1.1);
+  expect(num.floor(1.111, 2)).toBe(1.11);
+  expect(num.floor(1.119, 2)).toBe(1.11);
+});
+
+test("floor.step", () => {
+  expect(num.floor.step(11, 10)).toBe(10);
+  expect(num.floor.step(19, 10)).toBe(10);
+});
+
+test("limit", () => {
+  expect(num.limit(0, 1, 3)).toBe(1);
+  expect(num.limit(1, 1, 3)).toBe(1);
+  expect(num.limit(2, 1, 3)).toBe(2);
+  expect(num.limit(3, 1, 3)).toBe(3);
+  expect(num.limit(4, 1, 3)).toBe(3);
+});
+
 test("round", () => {
-  expect(num.round(1.111_111, 0)).toBe(1);
-  expect(num.round(1.111_111, 1)).toBe(1.1);
-  expect(num.round(1.111_111, 2)).toBe(1.11);
+  expect(num.round(1.111, 1)).toBe(1.1);
+  expect(num.round(1.199, 1)).toBe(1.2);
+  expect(num.round(1.111, 2)).toBe(1.11);
+  expect(num.round(1.119, 2)).toBe(1.12);
+});
+
+test("round.step", () => {
+  expect(num.round.step(11, 10)).toBe(10);
+  expect(num.round.step(19, 10)).toBe(20);
 });
 
 test("sum", () => {
