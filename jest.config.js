9720a6fab2cecec393571f61219aacec5ac6821e
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-03-29 02:40:48 +0300

    chore(jest): Do not collect coverage for .js files

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  // eslint-disable-next-line no-warning-comments
+  // fixme: Wait for @skylib/config update
+  collectCoverageFrom: ["src/**/*.{ts,tsx,vue}", "!**/*.d.ts"],
   preset: "@skylib/config/src",
   testEnvironment: "@skylib/config/src/jest-env-jsdom"
 };
