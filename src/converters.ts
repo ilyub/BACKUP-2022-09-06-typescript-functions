91a59712bb1c1190165dd4e5b3d50208ea8f6e01
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-03-31 22:08:09 +0300

    feat(converters): Removed multiple functions

--- src/converters.ts
+++ src/converters.ts
@@ -1,47 +1,6 @@
-import { ConversionError } from "./errors/ConversionError";
 import * as is from "./guards";
-import * as o from "./object";
 // eslint-disable-next-line @skylib/consistent-import
 import type * as types from "./types/core";
-
-export interface Converter<T> {
-  /**
-   * Converts value to type T.
-   *
-   * @param value - Value.
-   * @returns Converted value.
-   */
-  (value: unknown): T;
-}
-
-export interface MultiArgConverter<T, A extends unknown[]> {
-  /**
-   * Converts value to type T.
-   *
-   * @param value - Value.
-   * @param args - Arguments.
-   * @returns Converted value.
-   */
-  (value: unknown, ...args: A): T;
-}
-
-export type OrFail = typeof orFail;
-
-export const orFail = Symbol("OrFail");
-
-/**
- * Creates single-arg converter.
- *
- * @param converter - Multi-arg converter.
- * @param args - Rest arguments.
- * @returns Single-arg converter.
- */
-export function factory<T, A extends unknown[]>(
-  converter: MultiArgConverter<T, A>,
-  ...args: A
-): Converter<T> {
-  return (value): T => converter(value, ...args);
-}
 
 /**
  * Not implemented.
@@ -51,145 +10,7 @@ export function not(): never {
 }
 
 /**
- * Converts value to an array.
- *
- * @param value - Value.
- * @returns Value if it is an array, empty array otherwise.
- */
-export function array(value: unknown): types.unknowns {
-  return is.array(value) ? value : [];
-}
-
-/**
- * Converts value to type T[].
- *
- * @param value - Value.
- * @param converter - Converter for type T.
- * @returns An array of converted elements if value is an array, empty array otherwise.
- */
-export function arrayOf<T>(value: unknown, converter: Converter<T>): T[] {
-  return is.array(value) ? value.map(element => converter(element)) : [];
-}
-
-array.of = arrayOf;
-
-/**
- * Converts value to type T[].
- *
- * @param value - Value.
- * @param guard - Guard for type T.
- * @returns Filtered array if value is an array, empty array otherwise.
- */
-export function arrayOfFiltered<T>(value: unknown, guard: is.Guard<T>): T[] {
-  return is.array(value) ? value.filter(guard) : [];
-}
-
-arrayOf.filtered = arrayOfFiltered;
-
-/**
- * Converts value to type T[].
- *
- * @param value - Value.
- * @param guard - Guard for type T.
- * @returns Value if it is of type T[], empty array if value is empty.
- * @throws ConversionError otherwise.
- */
-export function arrayOfOrFail<T>(
-  value: unknown,
-  guard: is.Guard<T>
-): readonly T[] {
-  if (is.array.of(value, guard)) return value;
-
-  if (is.empty(value)) return [];
-
-  throw new ConversionError();
-}
-
-arrayOf.orFail = arrayOfOrFail;
-
-/**
- * Converts value to type T[].
- *
- * @param value - Value.
- * @param converter - Converter for type T | undefined.
- * @returns Filtered array of converted elements if value is an array, empty array otherwise.
- */
-export function arrayOfUndef<T>(
-  value: unknown,
-  converter: Converter<T | undefined>
-): T[] {
-  return is.array(value)
-    ? value.map(element => converter(element)).filter(is.not.empty)
-    : [];
-}
-
-arrayOf.undef = arrayOfUndef;
-
-/**
- * Converts value to a boolean.
- *
- * @param value - Value.
- * @returns Converted value.
- */
-export function boolean(value: unknown): boolean {
-  return Boolean(value);
-}
-
-/**
- * Converts value to type T.
- *
- * @param value - Value.
- * @param guard - Guard for type T.
- * @param defVal - Default value.
- * @returns Value if it is of type T, defVal otherwise.
- * @throws ConversionError if defVal is equal to orFail.
- */
-export function byGuard<T>(
-  value: unknown,
-  guard: is.Guard<T>,
-  defVal: OrFail | T
-): T {
-  if (guard(value)) return value;
-
-  if (defVal === orFail) throw new ConversionError();
-
-  return defVal;
-}
-
-/**
- * Not implemented.
- */
-export function callable(): never {
-  throw new Error("Not implemented");
-}
-
-/**
- * Converts value to type T.
- *
- * @param value - Value.
- * @returns Value if it is a Function.
- * @throws ConversionError otherwise.
- */
-export function callableOrFail<T extends Function>(value: unknown): T {
-  if (is.callable<T>(value)) return value;
-
-  throw new ConversionError();
-}
-
-callable.orFail = callableOrFail;
-
-/**
- * Converts value to type T | undefined.
- *
- * @param value - Value.
- * @returns Value if it is a Function, _undefined_ otherwise.
- */
-export function callableU<T extends Function>(value: unknown): T | undefined {
-  return is.callable<T>(value) ? value : undefined;
-}
-
-/**
- * Converts value to non-empty.
+ * Converts value to not empty.
  *
  * @param value - Value.
  * @param defVal - Default value.
@@ -205,171 +26,6 @@ export function notEmpty<T>(
 not.empty = notEmpty;
 
 /**
- * Converts value to type T.
- *
- * @param value - Value.
- * @param vo - Validation object.
- * @param defVal - Default value.
- * @returns Value if it is of type T, defVal otherwise.
- * @throws ConversionError if defVal is equal to orFail.
- */
-export function enumeration<T extends PropertyKey>(
-  value: unknown,
-  vo: types.ValidationObject<T>,
-  defVal: OrFail | T
-): T {
-  if (is.enumeration(value, vo)) return value;
-
-  if (defVal === orFail) throw new ConversionError();
-
-  return defVal;
-}
-
-/**
- * Converts value to type T | undefined.
- *
- * @param value - Value.
- * @param vo - Validation object.
- * @returns Value if it is of type T, _undefined_ otherwise.
- */
-export function enumerationU<T extends PropertyKey>(
-  value: unknown,
-  vo: types.ValidationObject<T>
-): T | undefined {
-  return is.enumeration(value, vo) ? value : undefined;
-}
-
-/**
- * Converts value to IndexedObject.
- *
- * @param value - Value.
- * @returns Value if it is an object, empty object otherwise.
- */
-export function indexedObject(value: unknown): types.ReadonlyIndexedObject {
-  return is.indexedObject(value) ? value : {};
-}
-
-/**
- * Converts value to IndexedObject\<T\>.
- *
- * @param value - Value.
- * @param converter - Converter for type T.
- * @returns Converted object if value is an object, empty object otherwise.
- */
-export function indexedObjectOf<T>(
-  value: unknown,
-  converter: Converter<T>
-): types.ReadonlyIndexedObject<T> {
-  return is.object(value)
-    ? o.fromEntries.exhaustive(
-        Object.entries(value).map(([k, v]) => [k, converter(v)])
-      )
-    : {};
-}
-
-indexedObject.of = indexedObjectOf;
-
-/**
- * Not implemented.
- */
-export function instance(): never {
-  throw new Error("Not implemented");
-}
-
-/**
- * Converts value to type T.
- *
- * @param value - Value.
- * @param ctor - Constructor.
- * @returns Value if it is a class instance.
- * @throws ConversionError otherwise.
- */
-export function instanceOrFail<T>(
-  value: unknown,
-  ctor: types.Constructor<T>
-): T {
-  if (value instanceof ctor) return value;
-
-  throw new ConversionError();
-}
-
-instance.orFail = instanceOrFail;
-
-/**
- * Converts value to type T | undefined.
- *
- * @param value - Value.
- * @param ctor - Constructor.
- * @returns Value if it is a class instance, _undefined_ otherwise.
- */
-export function instanceU<T>(
-  value: unknown,
-  ctor: types.Constructor<T>
-): T | undefined {
-  return value instanceof ctor ? value : undefined;
-}
-
-/**
- * Not implemented.
- */
-export function instances(): never {
-  throw new Error("Not implemented");
-}
-
-/**
- * Converts value to type T[].
- *
- * @param value - Value.
- * @param ctor - Constructor.
- * @returns Filtered array if value is an array, empty array otherwise.
- */
-export function instancesFiltered<T>(
-  value: unknown,
-  ctor: types.Constructor<T>
-): T[] {
-  return arrayOf.filtered(value, is.factory(is.instance, ctor));
-}
-
-instances.filtered = instancesFiltered;
-
-/**
- * Converts value to type T[].
- *
- * @param value - Value.
- * @param ctor - Constructor.
- * @returns Value if it is of type T[], empty array if value is empty.
- * @throws ConversionError otherwise.
- */
-export function instancesOrFail<T>(
-  value: unknown,
-  ctor: types.Constructor<T>
-): readonly T[] {
-  return arrayOf.orFail(value, is.factory(is.instance, ctor));
-}
-
-instances.orFail = instancesOrFail;
-
-/**
- * Converts value to NumStr.
- *
- * @param value - Value.
- * @returns Converted value.
- */
-export function numStr(value: unknown): types.NumStr {
-  return is.number(value) ? value : string(value);
-}
-
-/**
- * Converts value to NumStrU.
- *
- * @param value - Value.
- * @returns Converted value.
- */
-export function numStrU(value: unknown): types.NumStrU {
-  return is.number(value) ? value : stringU(value);
-}
-
-/**
  * Converts value to a number.
  *
  * @param value - Value.
@@ -379,23 +35,6 @@ export function numStrU(value: unknown): types.NumStrU {
 export function number(value: unknown, defVal = 0): number {
   return numberU(value) ?? defVal;
 }
-
-/**
- * Converts value to a number.
- *
- * @param value - Value.
- * @returns Converted value.
- * @throws ConversionError on failure.
- */
-export function numberOrFail(value: unknown): number {
-  const result = numberU(value);
-
-  if (is.not.empty(result)) return result;
-
-  throw new ConversionError();
-}
-
-number.orFail = numberOrFail;
 
 /**
  * Converts value to numberU.
@@ -425,16 +64,6 @@ export function numberU(value: unknown): types.numberU {
 }
 
 /**
- * Converts value to an object.
- *
- * @param value - Value.
- * @returns Value if it is an object, empty object otherwise.
- */
-export function object(value: unknown): object {
-  return is.object(value) ? value : {};
-}
-
-/**
  * Converts value to a string.
  *
  * @param value - Value.
@@ -445,40 +74,13 @@ export function string(value: unknown): string {
 }
 
 /**
- * Converts value to a string.
- *
- * @param value - Value.
- * @returns Converted value.
- * @throws ConversionError if value is converted to an empty string.
- */
-export function stringOrFail(value: unknown): string {
-  const result = string(value);
-
-  if (result) return result;
-
-  throw new ConversionError();
-}
-
-string.orFail = stringOrFail;
-
-/**
  * Converts value to stringU.
  *
  * @param value - Value.
  * @returns Converted value.
  */
 export function stringU(value: unknown): types.stringU {
-  const result = string(value);
+  const result = is.not.empty(value) ? String(value) : "";
 
   return result ? result : undefined;
-}
-
-/**
- * Do nothing converter.
- *
- * @param value - Value.
- * @returns Value.
- */
-export function unknown(value: unknown): unknown {
-  return value;
 }
--- tests/converters.ts
+++ tests/converters.ts
@@ -1,273 +1,14 @@
 import * as cast from "@/converters";
-import { ConversionError } from "@/errors/ConversionError";
-import * as is from "@/guards";
 import type { stringE } from "@/types/core";
-import { createValidationObject } from "@/types/core";
-
-class TestClass {
-  public value: number;
-
-  public constructor(value: number) {
-    this.value = value;
-  }
-}
-
-type TestEnum = "a" | 1;
-
-const TestEnumVO = createValidationObject<TestEnum>({ 1: 1, a: "a" });
-
-const testClass1 = new TestClass(1);
-
-const testClass2 = new TestClass(2);
-
-const testError = new ConversionError();
-
-function testFunction(): number {
-  return 1;
-}
-
-test("array", () => {
-  const arr = [1];
-
-  expect(cast.array([1])).toStrictEqual([1]);
-  expect(cast.array(1)).toStrictEqual([]);
-  expect(cast.array(arr)).toBeSameAs(arr);
-});
-
-test("array.of", () => {
-  const converter = cast.factory(cast.array.of, cast.number);
-
-  const arr = [1];
-
-  expect(converter([1])).toStrictEqual([1]);
-  expect(converter(["a"])).toStrictEqual([0]);
-  expect(converter(1)).toStrictEqual([]);
-  expect(converter(null)).toStrictEqual([]);
-  expect(converter(undefined)).toStrictEqual([]);
-  expect(converter(arr)).not.toBeSameAs(arr);
-});
-
-test("array.of.filtered", () => {
-  const converter = cast.factory(cast.array.of.filtered, is.number);
-
-  const arr = [1];
-
-  expect(converter([1])).toStrictEqual([1]);
-  expect(converter(["a"])).toStrictEqual([]);
-  expect(converter(1)).toStrictEqual([]);
-  expect(converter(null)).toStrictEqual([]);
-  expect(converter(undefined)).toStrictEqual([]);
-  expect(converter(arr)).not.toBeSameAs(arr);
-});
-
-test("array.of.orFail", () => {
-  const converter = cast.factory(cast.array.of.orFail, is.number);
-
-  const arr = [1];
-
-  expect(converter([1])).toStrictEqual([1]);
-  expect(() => converter(["a"])).toThrow(testError);
-  expect(() => converter(1)).toThrow(testError);
-  expect(converter(null)).toStrictEqual([]);
-  expect(converter(undefined)).toStrictEqual([]);
-  expect(converter(arr)).toBeSameAs(arr);
-});
-
-test("array.of.undef", () => {
-  const converter = cast.factory(cast.array.of.undef, cast.numberU);
-
-  const arr = [1];
-
-  expect(converter([1])).toStrictEqual([1]);
-  expect(converter(["a"])).toStrictEqual([]);
-  expect(converter(1)).toStrictEqual([]);
-  expect(converter(null)).toStrictEqual([]);
-  expect(converter(undefined)).toStrictEqual([]);
-  expect(converter(arr)).not.toBeSameAs(arr);
-});
-
-test("boolean", () => {
-  expect(cast.boolean(true)).toBeTrue();
-  expect(cast.boolean(false)).toBeFalse();
-  expect(cast.boolean({})).toBeTrue();
-  expect(cast.boolean([])).toBeTrue();
-  expect(cast.boolean("")).toBeFalse();
-  expect(cast.boolean(0)).toBeFalse();
-  expect(cast.boolean(-0)).toBeFalse();
-  expect(cast.boolean(Number.NaN)).toBeFalse();
-  expect(cast.boolean(null)).toBeFalse();
-  expect(cast.boolean(undefined)).toBeFalse();
-});
-
-test("byGuard", () => {
-  const converter1 = cast.factory(cast.byGuard, is.number, 2);
-
-  const converter2 = cast.factory(cast.byGuard, is.number, cast.orFail);
-
-  expect(converter1(1)).toBe(1);
-  expect(converter1("a")).toBe(2);
-  expect(() => converter2("a")).toThrow(testError);
-});
-
-test("callable", () => {
-  expect(() => cast.callable()).toThrow(new Error("Not implemented"));
-});
-
-test("callable.orFail", () => {
-  expect(cast.callable.orFail(testFunction)).toStrictEqual(testFunction);
-  expect(() => cast.callable.orFail(1)).toThrow(testError);
-});
-
-test("callableU", () => {
-  expect(cast.callableU(testFunction)).toStrictEqual(testFunction);
-  expect(cast.callableU(1)).toBeUndefined();
-});
-
-test("enumeration", () => {
-  const converter1 = cast.factory(cast.enumeration, TestEnumVO, 1);
-
-  const converter2 = cast.factory(cast.enumeration, TestEnumVO, cast.orFail);
-
-  expect(converter1(1)).toBe(1);
-  expect(converter1("a")).toBe("a");
-  expect(converter1("b")).toBe(1);
-  expect(() => converter2("b")).toThrow(testError);
-});
-
-test("enumerationU", () => {
-  const converter = cast.factory(cast.enumerationU, TestEnumVO);
-
-  expect(converter(1)).toBe(1);
-  expect(converter("a")).toBe("a");
-  expect(converter("b")).toBeUndefined();
-});
-
-test("indexedObject", () => {
-  const obj = { a: 1 };
-
-  expect(cast.indexedObject({ a: 1 })).toStrictEqual({ a: 1 });
-  expect(cast.indexedObject(1)).toStrictEqual({});
-  expect(cast.indexedObject(null)).toStrictEqual({});
-  expect(cast.indexedObject(undefined)).toStrictEqual({});
-  expect(cast.indexedObject(obj)).toBeSameAs(obj);
-});
-
-test("indexedObject.of", () => {
-  const converter = cast.factory(cast.indexedObject.of, cast.string);
-
-  const obj = { a: 1 };
-
-  expect(converter({ a: 1 })).toStrictEqual({ a: "1" });
-  expect(converter(1)).toStrictEqual({});
-  expect(converter(null)).toStrictEqual({});
-  expect(converter(undefined)).toStrictEqual({});
-  expect(converter(obj)).not.toBeSameAs(obj);
-});
-
-test("instance", () => {
-  expect(() => cast.instance()).toThrow(new Error("Not implemented"));
-});
-
-test("instance.orFail", () => {
-  const converter = cast.factory(cast.instance.orFail, TestClass);
-
-  expect(converter(testClass1)).toStrictEqual(testClass1);
-  expect(() => converter(1)).toThrow(testError);
-  expect(() => converter(null)).toThrow(testError);
-  expect(() => converter(undefined)).toThrow(testError);
-});
-
-test("instanceU", () => {
-  const converter = cast.factory(cast.instanceU, TestClass);
-
-  expect(converter(testClass1)).toStrictEqual(testClass1);
-  expect(converter(1)).toBeUndefined();
-  expect(converter(null)).toBeUndefined();
-  expect(converter(undefined)).toBeUndefined();
-});
-
-test("instances", () => {
-  expect(() => cast.instances()).toThrow(new Error("Not implemented"));
-});
-
-test("instances.filtered", () => {
-  const converter = cast.factory(cast.instances.filtered, TestClass);
-
-  const arr1 = [testClass1, testClass2];
-
-  const arr2 = [testClass1, testClass2, 1];
-
-  const expected = [testClass1, testClass2];
-
-  expect(converter(arr1)).toStrictEqual(expected);
-  expect(converter(arr2)).toStrictEqual(expected);
-  expect(converter(1)).toStrictEqual([]);
-  expect(converter(null)).toStrictEqual([]);
-  expect(converter(undefined)).toStrictEqual([]);
-  expect(converter(arr1)).not.toBeSameAs(arr1);
-});
-
-test("instances.orFail", () => {
-  const converter = cast.factory(cast.instances.orFail, TestClass);
-
-  const arr1 = [testClass1, testClass2];
-
-  const arr2 = [testClass1, testClass2, 1];
-
-  const expected = [testClass1, testClass2];
-
-  expect(converter(arr1)).toStrictEqual(expected);
-  expect(() => converter(arr2)).toThrow(testError);
-  expect(() => converter(1)).toThrow(testError);
-  expect(converter(null)).toStrictEqual([]);
-  expect(converter(undefined)).toStrictEqual([]);
-  expect(converter(arr1)).toBeSameAs(arr1);
-});
 
 test("not", () => {
   expect(() => cast.not()).toThrow(new Error("Not implemented"));
 });
 
 test("not.empty", () => {
-  expect(subtest("a")).toBe("a");
-  expect(subtest(null)).toBe("b");
-  expect(subtest(undefined)).toBe("b");
-
-  function subtest(value: stringE): string {
-    return cast.not.empty(value, "b");
-  }
-});
-
-test("numStr", () => {
-  expect(cast.numStr(123)).toBe(123);
-  expect(cast.numStr(-12.3)).toStrictEqual(-12.3);
-  expect(cast.numStr(true)).toBe("true");
-  expect(cast.numStr(false)).toBe("false");
-  expect(cast.numStr(" 12 ")).toBe(" 12 ");
-  expect(cast.numStr(" -12.34 ")).toBe(" -12.34 ");
-  expect(cast.numStr(" 12s ")).toBe(" 12s ");
-  expect(cast.numStr("\n")).toBe("\n");
-  expect(cast.numStr("\r\n")).toBe("\r\n");
-  expect(cast.numStr([])).toBe("");
-  expect(cast.numStr(null)).toBe("");
-  expect(cast.numStr(undefined)).toBe("");
-  expect(cast.numStr(undefined)).toBe("");
-});
-
-test("numStrU", () => {
-  expect(cast.numStrU(123)).toBe(123);
-  expect(cast.numStrU(-12.3)).toStrictEqual(-12.3);
-  expect(cast.numStrU(true)).toBe("true");
-  expect(cast.numStrU(false)).toBe("false");
-  expect(cast.numStrU(" 12 ")).toBe(" 12 ");
-  expect(cast.numStrU(" -12.34 ")).toBe(" -12.34 ");
-  expect(cast.numStrU(" 12s ")).toBe(" 12s ");
-  expect(cast.numStrU("\n")).toBe("\n");
-  expect(cast.numStrU("\r\n")).toBe("\r\n");
-  expect(cast.numStrU([])).toBeUndefined();
-  expect(cast.numStrU(null)).toBeUndefined();
-  expect(cast.numStrU(undefined)).toBeUndefined();
-  expect(cast.numStrU(undefined)).toBeUndefined();
+  expect(cast.not.empty<stringE>("a", "b")).toBe("a");
+  expect(cast.not.empty<stringE>(null, "b")).toBe("b");
+  expect(cast.not.empty<stringE>(undefined, "b")).toBe("b");
 });
 
 test("number", () => {
@@ -282,23 +23,9 @@ test("number", () => {
   expect(cast.number("\r\n")).toBe(0);
   expect(cast.number([])).toBe(0);
   expect(cast.number(null)).toBe(0);
+  expect(cast.number(null, 1)).toBe(1);
   expect(cast.number(undefined)).toBe(0);
   expect(cast.number(undefined, 1)).toBe(1);
-});
-
-test("number.orFail", () => {
-  expect(cast.number.orFail(123)).toBe(123);
-  expect(cast.number.orFail(-12.3)).toStrictEqual(-12.3);
-  expect(cast.number.orFail(true)).toBe(1);
-  expect(cast.number.orFail(false)).toBe(0);
-  expect(cast.number.orFail(" 12 ")).toBe(12);
-  expect(cast.number.orFail(" -12.34 ")).toStrictEqual(-12.34);
-  expect(() => cast.number.orFail(" 12s ")).toThrow(testError);
-  expect(() => cast.number.orFail("\n")).toThrow(testError);
-  expect(() => cast.number.orFail("\r\n")).toThrow(testError);
-  expect(() => cast.number.orFail([])).toThrow(testError);
-  expect(() => cast.number.orFail(null)).toThrow(testError);
-  expect(() => cast.number.orFail(undefined)).toThrow(testError);
 });
 
 test("numberU", () => {
@@ -316,11 +43,6 @@ test("numberU", () => {
   expect(cast.numberU(undefined)).toBeUndefined();
 });
 
-test("object", () => {
-  expect(cast.object({ a: 1 })).toStrictEqual({ a: 1 });
-  expect(cast.object(1)).toStrictEqual({});
-});
-
 test("string", () => {
   expect(cast.string(123)).toBe("123");
   expect(cast.string(-12.3)).toBe("-12.3");
@@ -332,17 +54,6 @@ test("string", () => {
   expect(cast.string(undefined)).toBe("");
 });
 
-test("string.orFail", () => {
-  expect(cast.string.orFail(123)).toBe("123");
-  expect(cast.string.orFail(-12.3)).toBe("-12.3");
-  expect(cast.string.orFail(true)).toBe("true");
-  expect(cast.string.orFail(false)).toBe("false");
-  expect(() => cast.string.orFail("")).toThrow(testError);
-  expect(() => cast.string.orFail([])).toThrow(testError);
-  expect(() => cast.string.orFail(null)).toThrow(testError);
-  expect(() => cast.string.orFail(undefined)).toThrow(testError);
-});
-
 test("stringU", () => {
   expect(cast.stringU(123)).toBe("123");
   expect(cast.stringU(-12.3)).toBe("-12.3");
@@ -352,8 +63,4 @@ test("stringU", () => {
   expect(cast.stringU([])).toBeUndefined();
   expect(cast.stringU(null)).toBeUndefined();
   expect(cast.stringU(undefined)).toBeUndefined();
-});
-
-test("unknown", () => {
-  expect(cast.unknown(123)).toBe(123);
 });
