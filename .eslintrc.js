fb2aab42f4b28a19fa0f877ce8a6f253167f2ed3
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2022-04-01 23:51:05 +0300

    chore(eslint): Enabled "@skylib/prefer-readonly" rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,29 @@
-module.exports = { extends: [require.resolve("@skylib/config/src/eslintrc")] };
+module.exports = {
+  extends: [require.resolve("@skylib/config/src/eslintrc")],
+  overrides: [
+    { files: ["tests/**"], rules: { "@skylib/prefer-readonly": "off" } }
+  ],
+  rules: {
+    "@skylib/no-mutable-signature": [
+      "warn",
+      {
+        ignoreClasses: true,
+        ignoreIdentifiers: [/^mutable/u.source],
+        ignoreInterfaces: true,
+        ignoreNumberSignature: true,
+        ignoreTypes: ["Promise", "ReadonlyMap", "ReadonlySet"]
+      }
+    ],
+    "@skylib/no-unnecessary-readonly": "warn",
+    "@skylib/no-unnecessary-writable": "warn",
+    "@skylib/prefer-readonly": [
+      "warn",
+      {
+        ignoreClasses: true,
+        ignoreIdentifiers: [/^mutable/u.source],
+        ignoreInterfaces: true,
+        ignoreTypes: ["Promise", "ReadonlyMap", "ReadonlySet"]
+      }
+    ]
+  }
+};
