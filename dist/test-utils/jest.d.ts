c001510adcad62f4af87bb6b5963b20086129100
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-06-23 23:03:46 +0300

    next

--- dist/test-utils/jest.d.ts
+++ dist/test-utils/jest.d.ts
@@ -1,4 +1,34 @@
 import "jest-extended";
+import type { Writable } from "..";
+import type { Calls } from "./expect.internal";
+declare global {
+    namespace jest {
+        interface Matchers<R> {
+            /**
+             * Checks that async function executes within expected time.
+             *
+             * @param expected - Expected time (ms).
+             * @param precision - Precision (ms).
+             * @returns Result.
+             */
+            readonly executionTimeToBe: (expected: number, precision?: number) => Promise<R>;
+            /**
+             * Checks that mock calls are equal to expected value.
+             *
+             * @param expected -- Calls.
+             * @returns Result.
+             */
+            readonly mockCallsToBe: (...expected: Writable<Calls>) => R;
+            /**
+             * Checks that two objects are identical.
+             *
+             * @param expected - Object.
+             * @returns Result.
+             */
+            readonly toBeSameAs: (expected: object) => R;
+        }
+    }
+}
 /**
  * Jest reset.
  */
