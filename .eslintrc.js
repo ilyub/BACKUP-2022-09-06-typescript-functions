a3a8582098a3e31a76ad2d7989f278d21d3e0726
Ilia Liubinskii <admin.phpdev@gmail.com>
2022-06-24 23:12:47 +0300

    chore(fix): Fix after "@skylib/config" update

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,9 @@ module.exports = {
   extends: [
     require.resolve("@skylib/config/src/eslintrc"),
     require.resolve("@skylib/config/src/eslintrc.allow-type-assertions"),
-    "./configs/eslintrc"
-  ],
-  overrides: [
-    {
-      extends: require.resolve(
-        "@skylib/config/src/eslintrc.skip-only-export-check"
-      ),
-      files: ["./src/guards.ts", "./src/types/object-keys.ts"]
-    },
-    { extends: ["./.eslintrc.overrides", "./.eslintrc.temp"], files: "**" }
+    "./configs/eslintrc",
+    "./.eslintrc.overrides",
+    "./.eslintrc.rule-overrides",
+    "./.eslintrc.temp"
   ]
 };
